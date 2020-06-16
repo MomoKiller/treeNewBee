7949dee6aee834f4273c1fa9a8293d48740473f4
MomoKiller <1226421458@qq.com>
2020-06-16 13:46:25 +0800

    更新总结文件

--- AES加密/加密使用方式.js
+++ AES加密/加密使用方式.js
@@ -89,3 +89,40 @@ decrypt(word){
 
 
 /* AES 其它模式类同 */ 
+
+/* aes 加密 */
+encrypt(word) {
+	// const key = 'GYTTv!z1bkA336GayE4F&Vu8b3WMsl!V'; // key
+	const key = CryptoJS.enc.Utf8.parse('GYTTv!z1bkA336GayE4F&Vu8b3WMsl!V'); //16位
+	let encrypted = '';
+	if (typeof (word) == 'string') {
+		let srcs = CryptoJS.enc.Utf8.parse(word);
+		encrypted = CryptoJS.AES.encrypt(srcs, key, {
+			mode: CryptoJS.mode.ECB,
+			padding: CryptoJS.pad.Pkcs7
+		});
+	} else if (typeof (word) == 'object') {//对象格式的转成json字符串
+		let data = JSON.stringify(word);
+		let srcs = CryptoJS.enc.Utf8.parse(data);
+		encrypted = CryptoJS.AES.encrypt(srcs, key, {
+			mode: CryptoJS.mode.ECB,
+			padding: CryptoJS.pad.Pkcs7
+		})
+	}
+	var res = base64.encode(encrypted.toString());
+	return res;
+}
+
+
+
+decrypt(word){
+	let word = base64.decode(word);
+	let key = CryptoJS.enc.Utf8.parse('GYTTv!z1bkA336GayE4F&Vu8b3WMsl!V');
+	let decrypt = CryptoJS.AES.decrypt(word, key, {
+		mode: CryptoJS.mode.ECB,
+		padding: CryptoJS.pad.Pkcs7
+	});
+	return decrypt.toString(CryptoJS.enc.Utf8);
+}
+
+
